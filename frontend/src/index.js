caeb4c32f8f3d4675412770ade992c0f326b6d04
Manmit Pal <manmitpal348@gmail.com>
2024-10-25 21:58:00 +0530

    Auth and User API Sice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,7 @@ import App from './App';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 
 
@@ -19,6 +20,7 @@ const router = createBrowserRouter(
        <Route index={true} path='/' element={<HomeScreen/>} ></Route>
        <Route path='/product/:id' element={<ProductScreen/>}></Route>
        <Route path='/cart' element={<CartScreen/>}></Route>
+       <Route path='/login' element={<LoginScreen/>}></Route>
     </Route>
   )
 )
